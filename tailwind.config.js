e76c16d1b3278137f35571098f1ded8797caecba
RuxCastillo <rubenldc1412@gmail.com>
2024-11-02 16:34:45 -0600

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	content: [
+		'./index.html',
+		'./src/**/*.{js,jsx,ts,tsx}', // Asegúrate de incluir las extensiones correctas
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
